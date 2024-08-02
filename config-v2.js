18329d0baa28436b93b73dd93736e5a740f523ed
ManuOfcTech <mdewni818@gmail.com>
2024-08-02 15:19:43 +0530

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,6 +1,6 @@
 const GITHUB_AUTH_TOKEN = "null"
 const BOT_NUMBER = "null"
-const SESSION_ID = ""
+const SESSION_ID = "PRABATH-MD~8vkhCa6b#YPE13YfhmHuJxwm_LBzAEjQKccvIUosdt_-fDGeSAC4"
 
 
 module.exports = {
